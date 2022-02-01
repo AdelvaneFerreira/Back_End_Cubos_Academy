d9118eb5319967b92ca1b9cde201d0af1205623a
Adelvane <adelvanefernandesfs@gmail.com>
2022-01-31 21:38:58 -0300

    Resolução dos exercícios 31-01-2022

--- Módulo-01/31-01-2022/07/index.js
+++ Módulo-01/31-01-2022/07/index.js
@@ -1,1 +1,13 @@
-Casos de COVID-19
+//Casos de COVID-19
+
+let populacaoInicial = 1000
+
+let qtdPacientes = 4
+
+let tempo = 100/7
+
+let pacientesInfectados = 0
+
+    pacientesInfectados = populacaoInicial * Math.pow( qtdPacientes,tempo)
+
+console.log(pacientesInfectados)
